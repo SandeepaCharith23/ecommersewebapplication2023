10526a5c9c22a90d507d8c74ea36f772bf56706a
Sandeepa Charith <74694596+SandeepaCharith23@users.noreply.github.com>
2023-10-18 08:34:30 +0530

    Turn Services Section into Swiper section
    Added Swiper.js to the file structure and apply Swiper function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ menubutton.onclick=()=>{
     menubutton.classList.toggle('fa-times');
     navbar.classList.toggle('active-navbar');
 }
+
+navbar.onclick=()=>{
+    navbar.classList.remove('active-navbar');
+    menubutton.classList.remove('fa-times');
+}
+
+menubutton.onscroll=()=>{
+    menubutton.classList.remove('fa-times');
+    navbar.classList.remove('active-navbar');
+}
