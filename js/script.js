fe8c64399d77057e4f1b3fb03f3fa51fcc31b131
Sandeepa Charith <74694596+SandeepaCharith23@users.noreply.github.com>
2023-10-20 22:10:03 +0530

    Service section and About Us section updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 let menubutton=document.querySelector('#menu-btn');
 let navbar=document.querySelector('.header .navbar');
+let searchForm=document.querySelector('.header .search-form');//select the searchbar
+let loginForm=document.querySelector('.header .login-form');//select the loginform
 
 menubutton.onclick=()=>{
     menubutton.classList.toggle('fa-times');
     navbar.classList.toggle('active-navbar');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
 }
+
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active-navbar');
+    loginForm.classList.remove('active');
+};
+
+document.querySelector('#login-btn').onclick=()=>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active-navbar');
+    searchForm.classList.remove('active');
+};
+
 
 navbar.onclick=()=>{
     navbar.classList.remove('active-navbar');
